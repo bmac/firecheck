245746e6a08c1ce8fcad97907dbe9f6345d55ed5
bmac <bmac325@GMail.com>
2014-04-07 22:04:53 -0400

    allow a user to join a room from the main screen

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,10 @@
 export default Ember.Route.extend({
   model: function() {
-    return this.store.find('message');
+    return 'foo';
   },
   actions: {
-    sendMessage: function(msg) {
-      var appController = this.controllerFor('application');
-      var nick = appController.get('nick');
-      var time = new Date();
-      
-      var message = this.store.createRecord('message', {
-        body: msg,
-        nick: nick,
-        time: time
-      });
-      message.save();
-      var indexController = this.controllerFor('index');
-      indexController.set('msg', '');
+    joinRoom: function(roomName) {
+      this.transitionTo('room', roomName);
     }
   }
 });
