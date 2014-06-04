1bb3fc3e1696bf4d1d5a1a23352a483f8314aabb
Brendan McLoughlin <btmcloughlin@gmail.com>
2014-06-03 21:32:45 -0400

    Update router.js

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 var Router = Ember.Router.extend({
   location: 'history'
-}); // ensure we don't share routes between all Router instances
+});
 
 Router.map(function() {
   this.route('room', {path: 'room/:room_name'});
